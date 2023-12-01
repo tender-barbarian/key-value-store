fbda70a6882596a41e0f4f7187b754427c88f0dd
solnicki <solnicki@logmanager.pl>
2023-12-01 12:57:12 +0100

    first generation

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"key-value-store/api"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err = api.Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := api.Get(key)
+	if errors.Is(err, api.ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
